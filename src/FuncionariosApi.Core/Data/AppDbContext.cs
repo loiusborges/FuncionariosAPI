422a85049c755585e885214d1db01e0ca5121c67
Luis Fernando <loiusborges@gmail.com>
2025-07-20 23:34:06 -0300

    Configuração do bando de dados e migrations

---
+++ src/FuncionariosApi.Core/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FuncionariosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuncionariosAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Funcionarios> Funcionarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("DataSource=app.db;Cache=shared");
+    }
+}
---
+++ src/FuncionariosApi.Core/Migrations/20250720235815_InitalCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FuncionariosAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Cargo = table.Column<string>(type: "TEXT", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Salario = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+        }
+    }
+}
